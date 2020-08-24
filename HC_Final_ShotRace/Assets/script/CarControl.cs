5246c62c681420b30336ef58640e0a5c1bf5197b
SeanGau <rrtw0627@gmail.com>
2020-08-24 17:19:57 +0800

    testcar can run nowgit add -A

---
+++ HC_Final_ShotRace/Assets/script/CarControl.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+    public WheelCollider wheel_fr;
+    public WheelCollider wheel_fl;
+    public WheelCollider wheel_br;
+    public WheelCollider wheel_bl;
+    public float accel = 1000f;
+    public float steer = 20;
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      rb.AddForce(0, -1*accel, 0);
+      wheel_fr.motorTorque = wheel_fl.motorTorque = wheel_br.motorTorque = wheel_bl.motorTorque = accel;
+
+      if(Input.GetKey(KeyCode.RightArrow))
+      {
+        wheel_fr.steerAngle = wheel_fl.steerAngle = Mathf.Lerp(wheel_fr.steerAngle, steer, 0.9f*Time.deltaTime);
+      }
+      else if(Input.GetKey(KeyCode.LeftArrow))
+      {
+        wheel_fr.steerAngle = wheel_fl.steerAngle = Mathf.Lerp(wheel_fr.steerAngle, -1*steer, 0.9f*Time.deltaTime);
+      }
+      else
+      {
+        wheel_fr.steerAngle = wheel_fl.steerAngle = 0;
+      }
+    }
+}
---
+++ HC_Final_ShotRace/Assets/script/CarPlayCamControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarPlayCamControl : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 30, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+      transform.position = target.position+offset;
+      //transform.position = Vector3.Lerp(transform.position, pos+offset, 0.7f * Time.deltaTime * 10);
+
+    }
+}
