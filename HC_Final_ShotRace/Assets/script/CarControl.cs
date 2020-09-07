3dcfed12a76f76d342a7dd9de9a0940199de3066
SeanGau <rrtw0627@gmail.com>
2020-09-07 21:34:11 +0800

    fix some turning issue

--- HC_Final_ShotRace/Assets/script/CarControl.cs
+++ HC_Final_ShotRace/Assets/script/CarControl.cs
@@ -9,6 +9,8 @@ public class CarControl : MonoBehaviour
     public WheelCollider wheel_fl;
     public WheelCollider wheel_br;
     public WheelCollider wheel_bl;
+    public Transform wheel_fr_obj;
+    public Transform wheel_fl_obj;
     public float accel = 2000f;
     public float breakv = 5000f;
     public float steer = 20;
@@ -21,9 +23,12 @@ public class CarControl : MonoBehaviour
     private Rigidbody rb;
     private float nowTorque = 0;
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+    }
+    void Start()
+    {
         wheel_br.brakeTorque = wheel_bl.brakeTorque = wheel_fr.brakeTorque = wheel_fl.brakeTorque = 0;
     }
 
@@ -43,44 +48,44 @@ public class CarControl : MonoBehaviour
     }
     private void Move()
     {
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            nowTorque = Mathf.Lerp(nowTorque, accel, 0.5f * Time.deltaTime * 10);
-        }
-        else
-        {
-            nowTorque = Mathf.Lerp(nowTorque, 0, 0.9f * Time.deltaTime * 10);
-        }
+        float v = Input.GetAxis("Vertical");
+        nowTorque = Mathf.Lerp(nowTorque, accel * v, 0.5f * Time.deltaTime * 10);
         wheel_br.motorTorque = wheel_bl.motorTorque = wheel_fr.motorTorque = wheel_fl.motorTorque = nowTorque;
         rb.AddForce(0, -1 * nowTorque, 0);
 
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            wheel_br.brakeTorque = wheel_bl.brakeTorque = wheel_fr.brakeTorque = wheel_fl.brakeTorque = breakv*1000;
-            rb.AddForce(rb.velocity* -breakv);
+            print(rb.velocity.sqrMagnitude);
+            if (rb.velocity.sqrMagnitude > 1)
+            {
+                wheel_br.brakeTorque = wheel_bl.brakeTorque = wheel_fr.brakeTorque = wheel_fl.brakeTorque = breakv * 1000;
+                rb.AddForce(rb.velocity * -breakv);
+            }
+            else
+            {
+                wheel_br.brakeTorque = wheel_bl.brakeTorque = wheel_fr.brakeTorque = wheel_fl.brakeTorque = 0;
+            }
         }
         else
         {
             wheel_br.brakeTorque = wheel_bl.brakeTorque = wheel_fr.brakeTorque = wheel_fl.brakeTorque = 0;
             rb.AddForce(rb.velocity* -10);
         }
-
-        if (Input.GetKey(KeyCode.RightArrow))
+        float h = Input.GetAxis("Horizontal");
+        wheel_fr.steerAngle = wheel_fl.steerAngle =  h * steer;
+        wheel_fl_obj.rotation = wheel_fr_obj.rotation = Quaternion.Euler(0, h * steer, 0);
+        /*
+        if (h!=0)
         {
-            wheel_fr.steerAngle = wheel_fl.steerAngle = Mathf.Lerp(wheel_fr.steerAngle, steer, 0.7f * Time.deltaTime * 10);
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            wheel_fr.steerAngle = wheel_fl.steerAngle = Mathf.Lerp(wheel_fr.steerAngle, -1 * steer, 0.7f * Time.deltaTime * 10);
+            wheel_fr.steerAngle = wheel_fl.steerAngle = Mathf.Lerp(wheel_fr.steerAngle, h*steer, 0.9f * Time.deltaTime * 10);
         }
         else
         {
-            wheel_fr.steerAngle = wheel_fl.steerAngle = Mathf.Lerp(wheel_fr.steerAngle, 0, 0.9f * Time.deltaTime *10);
-        }
+            wheel_fr.steerAngle = wheel_fl.steerAngle = Mathf.Lerp(wheel_fr.steerAngle, 0, 0.9f * Time.deltaTime *10);            
+        }*/
 
 
-        if(transform.position.y < -1)
+        if (transform.position.y < -1)
         {
             transform.position = new Vector3(0, 1, 0);
         }
