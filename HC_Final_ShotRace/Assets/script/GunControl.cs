914ba178af2a9aff9ca764223861fbce564d9d9a
keven11472 <67904682+keven11472@users.noreply.github.com>
2020-09-08 20:16:21 +0800

    完成武器FPS

---
+++ HC_Final_ShotRace/Assets/script/GunControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class GunControl : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+    public Transform Gun;
+
+    //float xRotation = 0f;
+
+    public Animator ani;
+
+    /// <summary>
+    /// 射擊
+    /// </summary>
+    private void shot()
+    {
+        bool leftmouse = Input.GetKey(KeyCode.Mouse0);
+        ani.SetBool("射擊", leftmouse);
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        //float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //xRotation += mouseY;
+        //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        //transform.localRotation = Quaternion.Euler(0f, xRotation, 0f);
+
+        Gun.Rotate(Vector3.right * mouseX);
+        //Gun.Rotate(Vector3.back * mouseY);
+
+        shot();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
