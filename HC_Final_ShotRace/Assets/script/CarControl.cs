df004cc5bf780993129b93ca20b857b596761e76
SeanGau <rrtw0627@gmail.com>
2020-09-09 21:38:56 +0800

    map nav line

--- HC_Final_ShotRace/Assets/script/CarControl.cs
+++ HC_Final_ShotRace/Assets/script/CarControl.cs
@@ -14,6 +14,7 @@ public class CarControl : MonoBehaviour
     public float accel = 4000f;
     public float breakv = 200f;
     public float steer = 30;
+    public float speedLimit = 100f;
 
     static int checkcount = 0;
     public Text checkText;
@@ -23,7 +24,6 @@ public class CarControl : MonoBehaviour
     private Rigidbody rb;
     private float nowTorque = 0;
     private int nowGear = 0; // P: 0, D: 1, R: -1
-    // Start is called before the first frame update
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -33,13 +33,9 @@ public class CarControl : MonoBehaviour
         wheel_br.brakeTorque = wheel_bl.brakeTorque = wheel_fr.brakeTorque = wheel_fl.brakeTorque = 0;
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        Move();
-    }
     void Update()
     {
+        Move();
         UpdateDashboard();
     }
     void UpdateDashboard()
@@ -49,6 +45,7 @@ public class CarControl : MonoBehaviour
         int dSpeed = (int)Mathf.Sqrt(rb.velocity.x * rb.velocity.x + rb.velocity.z * rb.velocity.z);
         speedText.text = dSpeed.ToString();
         torqueImage.fillAmount = Mathf.Abs(nowTorque) / accel * 0.5f;
+
     }
     private void Move()
     {
@@ -65,31 +62,34 @@ public class CarControl : MonoBehaviour
         {
           nowGear = 0;
         }
-        rb.AddForce(-transform.up * localVelocity.sqrMagnitude);
+        rb.AddForce(-transform.up * localVelocity.sqrMagnitude * Time.deltaTime * 50);
 
         float v = Input.GetAxis("Vertical");
-        if (v == 0)
+        if (v == 0) //滑行
         {
-            rb.AddForce(rb.velocity * -breakv);
+            rb.AddForce(rb.velocity * -breakv * Time.deltaTime);
             nowTorque = Mathf.Lerp(nowTorque, 0, 0.5f);
             wheel_br.brakeTorque = wheel_bl.brakeTorque = wheel_fr.brakeTorque = wheel_fl.brakeTorque = 0;
         }
-        else if (localVelocity.z * v > 0 || nowGear == 0)
+        else if (localVelocity.z * v > 0 || nowGear == 0) //加速or低速狀態
         {
             nowTorque = Mathf.Lerp(nowTorque, accel * v, 0.5f);
             wheel_br.brakeTorque = wheel_bl.brakeTorque = wheel_fr.brakeTorque = wheel_fl.brakeTorque = 0;
         }
         else
         {
-            if (localVelocity.z * v < 0)
+            if (localVelocity.z * v < 0) //切換方向
             {
-              rb.AddForce(rb.velocity * -breakv);
+              rb.AddForce(rb.velocity * -breakv * Time.deltaTime * 50);
             }
             nowTorque = Mathf.Lerp(nowTorque, 0, 0.5f);
             wheel_br.brakeTorque = wheel_bl.brakeTorque = breakv * 1000;
         }
-        wheel_br.motorTorque = wheel_bl.motorTorque = wheel_fr.motorTorque = wheel_fl.motorTorque = nowTorque;
-
+        int dSpeed = (int)Mathf.Sqrt(rb.velocity.x * rb.velocity.x + rb.velocity.z * rb.velocity.z);
+        if (dSpeed < speedLimit)
+            wheel_br.motorTorque = wheel_bl.motorTorque = wheel_fr.motorTorque = wheel_fl.motorTorque = nowTorque;
+        else
+            wheel_br.motorTorque = wheel_bl.motorTorque = wheel_fr.motorTorque = wheel_fl.motorTorque = 0;
         float h = Input.GetAxis("Horizontal");
         wheel_fr.steerAngle = wheel_fl.steerAngle =  h * steer;
 
--- HC_Final_ShotRace/Assets/script/SmallMapper.cs
+++ HC_Final_ShotRace/Assets/script/SmallMapper.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public class SmallMapper : MonoBehaviour
 {
-    public Texture aTexture;
+    public LineRenderer lr;
+    public Transform target;
+    public Transform player;
+
+    public GameObject smallMap;
+    public GameObject bigMap;
+    private bool toggleMap = false;
+
+    private void LateUpdate()
+    {
+        lr.SetPositions(new Vector3[]{ target.position, player.position});
+        
+        Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float angle = Mathf.Atan((mousepos.y-0.4f) / (mousepos.x-0.4f))* Mathf.Rad2Deg;
+        print("mouse: " + mousepos + " angle: "+ angle);
+
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            smallMap.SetActive(toggleMap);
+            toggleMap = !toggleMap;
+            float lwidth = toggleMap ? 5f:1.5f;
+            lr.startWidth = lr.endWidth = lwidth;
+            bigMap.SetActive(toggleMap);
+        }
+    }
 }
