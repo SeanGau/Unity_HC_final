4c094b34a3632d6018ca2b5d27f338c9cd0ac309
ShuiDi <67904682+keven11472@users.noreply.github.com>
2020-10-19 20:18:44 +0800

    修改好飛彈角度

--- HC_Final_ShotRace/Assets/script/MessileControl.cs
+++ HC_Final_ShotRace/Assets/script/MessileControl.cs
@@ -44,9 +44,19 @@ public class MessileControl : GunBase
     }
     private void Mouse()
     {
-        Vector3 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
-        Vector3 targetPos = new Vector3(mousePos.x - 0.5f, 90, mousePos.y - 0.5f);
-        Gun.forward = targetPos;
+        //Vector3 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        //Vector3 targetPos = new Vector3(mousePos.x - 0.5f, 90, mousePos.y - 0.5f);
+        //Gun.forward = targetPos;
+
+        Vector3 posMouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        Vector3 posWorld = Camera.main.ScreenToWorldPoint(posMouse);
+        //Vector3 posMouse = new Vector3(posWorld.x - 0.5f, 0, posWorld.y - 0.5f);
+
+        posWorld.y = transform.position.y;
+
+        Vector3 direction = posWorld - transform.position;
+
+        transform.forward = direction;
     }
 
     protected override void Update()
